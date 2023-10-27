65508b6a25c59431e0b47f931ec7fd91e1f2e5b1
William <WilliamMTriDo@gmail.com>
2023-10-26 17:51:05 -0700

    Rewriting test

--- test/test.cpp
+++ test/test.cpp
@@ -7,3 +7,15 @@ TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
 }
+
+TEST(TriangleTests, testKind) {
+    Triangle *newTriangle = new Triangle(4,3,2);
+    EXPECT_EQ (newTriangle->getKind(),Triangle::Kind::SCALENE);
+}
+
+TEST(TriangleTests, testArea) {
+    Triangle *aTriangle = new Triangle(6,5,5);
+    EXPECT_EQ (aTriangle->getArea(),12);
+}
+
+
