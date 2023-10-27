6c11d321aa0353b3f0a7f635f382e40526297001
dmasequesmay <dominique.masequesmay@gmail.com>
2023-10-27 14:06:24 -0700

    Add 4 tests that fail and 4 tests that pass

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,53 @@
 #include "gtest/gtest.h"
 #include "../include/Triangle.h"
+#include <cmath>
 using shapes::Triangle;
 
+//fails
+TEST(TriangleTests, testKindEQUILATERAL) {
+    Triangle *aTriangle = new Triangle(3,3,3);
+    EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::EQUILATERAL);
+}
+
+//fails
+TEST(TriangleTests,testAreaIsoceles) {
+    Triangle *aTriangle = new Triangle(3,3,1);
+    EXPECT_EQ (aTriangle->getArea(), sqrt(2.1875));
+}
+
+//fails
+TEST(TriangleTests,testPerimeterIsoceles) {
+    Triangle *aTriangle = new Triangle(3,3,1);
+    EXPECT_EQ (aTriangle->getArea(), 7);
+}
+
+//fails
+TEST(TriangleTests, expectDeath) {
+    EXPECT_DEATH (Triangle *aTriangle = new Triangle(1,5,5), "First side not longest");
+}
 
 TEST(TriangleTests, testPerimeter) {
     Triangle *aTriangle = new Triangle(3,3,3);
     EXPECT_EQ (aTriangle->getPerimeter(),9);
 }
+
+TEST(TriangleTests, testArea) {
+    Triangle *aTriangle = new Triangle(6,5,5);
+    EXPECT_EQ (aTriangle->getArea(), 12);
+}
+
+TEST(TriangleTests, testKindIsosceles) {
+    Triangle *aTriangle = new Triangle(3,2,2);
+    ASSERT_EQ (aTriangle->getKind(), Triangle::Kind::ISOSCELES);
+}
+
+TEST(TriangleTests, testKindScalene) {
+    Triangle *aTriangle = new Triangle(5,4,3);
+    EXPECT_EQ (aTriangle->getKind(), Triangle::Kind::SCALENE);
+}
+
+TEST(TriangleTests, expectNotThrow) {
+    Triangle *aTriangle = new Triangle(3,3,1);
+    EXPECT_NO_THROW (aTriangle->getPerimeter());
+}
+
