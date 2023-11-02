52f9f1b604a222b9ef0ad687b9ccabd1e0e7f6f0
dmasequesmay <dominique.masequesmay@gmail.com>
2023-11-02 14:55:25 -0700

    Rename test/test.cpp to test/hailstoneTests.cpp

---
+++ test/hailstoneTests.cpp
@@ -0,0 +1,20 @@
+#include "gtest/gtest.h"
+#include "../include/Hailstone.h"
+using sequence::satisfiesHailstone;
+
+
+TEST(HailstoneTests, number0) {
+    EXPECT_EQ (sequence::satisfiesHailstone(0), false);
+}
+
+TEST(HailstoneTests, number1) {
+    EXPECT_EQ (sequence::satisfiesHailstone(1), true);
+}
+
+TEST(HailstoneTests, number2) {
+    EXPECT_EQ (sequence::satisfiesHailstone(2), true);
+}
+
+TEST(HailstoneTests, number3) {
+    EXPECT_EQ (sequence::satisfiesHailstone(3), true);
+}
