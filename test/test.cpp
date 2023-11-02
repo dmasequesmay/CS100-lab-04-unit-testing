56f87c58ae2fd80f7f4b69d1dc29de2fcb83feaf
dmasequesmay <dominique.masequesmay@gmail.com>
2023-11-02 14:42:00 -0700

    Add Google Mock Tests for AwardTests

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,49 @@
 #include "gtest/gtest.h"
-#include "../include/Triangle.h"
-using shapes::Triangle;
+#include <gmock/gmock.h>
+#include <vector>
+#include "../include/Awards.h"
 
 
-TEST(TriangleTests, testPerimeter) {
-    Triangle *aTriangle = new Triangle(3,3,3);
-    EXPECT_EQ (aTriangle->getPerimeter(),9);
-}
+using awards::RankList;
+using awards::AwardCeremonyActions;
+
+class MockRankList : public RankList {
+    private:
+    std::vector<std::string> names;
+    int index;
+
+    public: 
+    std::string getNext() {
+        return names.at(index++);
+    }
+    MockRankList() : names{"Adam", "Bob", "Charlie"}, index(0) {}
+};
+
+class MockAwardCeremonyActions : public AwardCeremonyActions {
+    public: 
+    MOCK_METHOD(void, playAnthem, (), (override));
+    MOCK_METHOD(void, turnOffTheLightsAndGoHome, (), (override));
+    MOCK_METHOD(void, awardBronze, (std::string recipient), (override));
+    MOCK_METHOD(void, awardSilver, (std::string recipient), (override));
+    MOCK_METHOD(void, awardGold, (std::string recipient), (override));
+};
+
+using::testing::InSequence;
+
+TEST(AwardsTest, FunctionsRunInOrder) {
+
+    MockRankList recipients;
+    MockAwardCeremonyActions actions;
+
+    InSequence seq;
+    {
+    EXPECT_CALL(actions, playAnthem());
+    EXPECT_CALL(actions, awardBronze("Adam"));
+    EXPECT_CALL(actions, awardSilver("Bob"));
+    EXPECT_CALL(actions, awardGold("Charlie"));
+    EXPECT_CALL(actions, turnOffTheLightsAndGoHome());
+    }
+
+    performAwardCeremony(recipients, actions);
+
+};
