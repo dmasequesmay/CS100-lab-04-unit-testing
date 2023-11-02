35c04b2a34878dc2af9612a304cee8ca848c840a
William <WilliamMTriDo@gmail.com>
2023-11-02 14:29:10 -0700

    Updated Tests for AwardCeremony

--- test/test.cpp
+++ test/test.cpp
@@ -4,23 +4,41 @@
 #include <vector>
 #include <string>
 using::testing::InSequence;
+using::testing::AtLeast;
 using awards::RankList;
 using awards::AwardCeremonyActions;
 
 class MockList : public RankList {
     private:
-        std::string getNext();
         int index = 0;
-        vector<std::string> names = {"Alex", "Bill", "Carly"}; 
-}
+        std::vector<std::string> names; 
+    public:
+        MOCK_METHOD(std::string, getNext, (), (override));
+        MockList() : names{"Alex", "Bill", "Carly"}, index(0) {}
+};
 
 class MockCeremony : public AwardCeremonyActions {
-    MOCK_METHOD(void, playAnthem, (), (override));
-    MOCK_METHOD(void, awardBronze, (recipients.getNext()), (override));
-    MOCK_METHOD(void, awardSilver, (recipients.getNext()), (override));
-    MOCK_METHOD(void, awardGold, (recipients.getNext()),(override));
-    MOCK_METHOD(void, turnOffTheLightsAndGoHome, (), (override));
+    public:
+        MOCK_METHOD(void, playAnthem, (), (override));
+        MOCK_METHOD(void, awardBronze, (std::string recipients), (override));
+        MOCK_METHOD(void, awardSilver, (std::string recipients), (override));
+        MOCK_METHOD(void, awardGold, (std::string recipients),(override));
+        MOCK_METHOD(void, turnOffTheLightsAndGoHome, (), (override));
+};
+
+TEST(AwardsTest, CeremonyInSequence) {
+    MockList recipients;
+    MockCeremony actions;
+
+    InSequence  seq;
+
+    EXPECT_CALL(actions, playAnthem());
+    EXPECT_CALL(actions, awardBronze("Alex"));
+    EXPECT_CALL(actions, awardSilver("Bill"));
+    EXPECT_CALL(actions, awardGold("Carly"));
+    EXPECT_CALL(actions, turnOffTheLightsAndGoHome());
 }
+
 
 
 
